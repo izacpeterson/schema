c0ac1cf1efdaff17241da9762efb8b401b4c352a
Izac Peterson <ipeterson@bhqut.local>
2022-11-25 13:26:29 -0700

    faq generator

---
+++ src/views/FAQView.vue
@@ -0,0 +1,30 @@
+<template>
+    <main class="flex flex-col p-5 items-start">
+        <h2 class="text-2xl">FAQ Schema</h2>
+        <div v-for="question in schema.mainEntity" class="m-2">
+            <label for="" class="m-2">Question: <input type="text" v-model="question.name" class="bg-gray-200 p-1"></label>
+            <label for="" class="m-2">Answer: <input type="text" v-model="question.acceptedAnswer.text" class="bg-gray-200 p-1"></label>
+        </div>
+        <button @click="addQuestion" class="bg-blue-500 text-white p-2">Add Question</button>
+    </main>
+    <pre class="bg-gray-200">{{`<script type="application/ld+json">`}} {{ schema }} {{`</script>`}} </pre>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            // data
+            schema: {
+                "@context": "https://schema.org/",
+                "@type": "FAQPage",
+                mainEntity: []
+            }
+        }
+    },
+    methods: {
+        addQuestion() {
+            this.schema.mainEntity.push({ "@type": "Question", "name":'', "acceptedAnswer": { "@type": "Answer" } })
+        }
+    }
+}
+</script>
--- src/views/HowToView.vue
+++ src/views/HowToView.vue
@@ -2,7 +2,8 @@
 </script>
 
 <template>
-  <main class="flex flex-col p-5">
+  <main class="flex flex-col p-5 items-start">
+    <h2 class="text-2xl">How To Schema</h2>
     <label for="" class="m-2">Name: <input type="text" v-model="schema.name" class="bg-gray-200 p-1"></label>
     <label for="" class="m-2">Total Time: <input type="text" v-model="schema.totalTime" class="bg-gray-200 p-1"></label>
     <div v-for="step in schema.step" class="m-2">
@@ -10,9 +11,9 @@
       <label for="" class="m-2">Step Text: <input type="text" v-model="step.text" class="bg-gray-200 p-1"></label>
       <label for="" class="m-2">Step Image URL: <input type="text" v-model="step.image" class="bg-gray-200 p-1"></label>
     </div>
-    <button @click="addStep" class="bg-blue-500">Add Step</button>
-    <pre>{{ schema }}</pre>
+    <button @click="addStep" class="bg-blue-500 text-white p-2">Add Step</button>
   </main>
+  <pre class="bg-gray-200">{{`<script type="application/ld+json">`}} {{ schema }} {{`</script>`}} </pre>
 </template>
 <script>
 export default {
